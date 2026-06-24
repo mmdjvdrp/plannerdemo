b553dd6108c4923bba5ce69edbfc4e6dd5f5d5a0
mmdjvdrp <mohammadjrahimpour@gmail.com>
2026-06-24 12:32:01 +0330

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,7 @@
 // js/supabase.js
-// ایمپورت صحیح کتابخانه سوپابیس از CDN اصلی
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const supabaseUrl = "https://ipureiqnhgatigewbggj.supabase.co"; // آدرس پروژه شما (درست است)
-const supabaseKey = "sb_publishable_ieckfcaUPxIeHPloSuR-rA_r7BC3aso"; // کلید عمومی شما (درست است)
+const supabaseUrl = "https://ipureiqnhgatigewbggj.supabase.co"; 
+const supabaseKey = "sb_publishable_ieckfcaUPxIeHPloSuR-rA_r7BC3aso"; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
